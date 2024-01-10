69c8456f7a384b562292e48622d55f3481cd328c
Justin Odhiambo <justinsibudiodhiambo@gmail.com>
2024-01-10 11:49:17 -0500

    Completed Assignment

--- lib/models.py
+++ lib/models.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 
-from sqlalchemy import (Column, String, Integer)
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 class Dog(Base):
     __tablename__ = 'dogs'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    breed = Column(String)
+    age = Column(Integer)
 
-    id = Column(Integer(), primary_key=True)
-    name = Column(String())
-    breed = Column(String())
+    def __repr__(self):
+        return f"<Dog(name={self.name}, breed={self.breed}, age={self.age})>"
